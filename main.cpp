3dfa920832ca128273dea697bca32bb8280b3b0b
msqrt <pauli.kemppinen@aalto.fi>
2022-01-16 21:33:46 +0200

    triangle main

--- main.cpp
+++ main.cpp
@@ -1,28 +1,48 @@
 
-#include <vector>
 #include "ssgl.h"
+#include "gl_timing.h"
 
 int main() {
-    OpenGL context(640, 480, "Simple", false, false);
-    
-    Buffer iota;
-    glNamedBufferData(iota, 1024 * 1024 * sizeof(uint32_t), nullptr, GL_STATIC_DRAW);
+    OpenGL context(1280, 720, "Triangle");
 
-    auto generator = [&] {
-        layout (local_size_x = 256) in;
-        buffer bind_block(iota) {
-            dynamic_array(uint, result);
-        };
-        void glsl_main() {
-            result[gl_GlobalInvocationID.x] = gl_GlobalInvocationID.x;
-        }
+    struct Vertex { vec3 position, color; };
+    Vertex verts[] = {
+        {vec3(.5f, .0f, .0f), vec3(1.f, .0f, .0f)},
+        {vec3(-.5f, .5f, .0f), vec3(.0f, 1.f, .0f)},
+        {vec3(-.5f,-.5f, .0f), vec3(.0f, .0f, 1.f)}
     };
 
-    useShader(generator());
-    glDispatchCompute(1024*4, 1, 1);
+    Buffer b; glNamedBufferData(b, sizeof(Vertex) * 3, verts, GL_STATIC_DRAW);
+    Attribute<vec3> position(b, sizeof(Vertex), 0), color(b, sizeof(Vertex), sizeof(vec3));
 
-    std::vector<uint32_t> result(1024 * 1024);
-    glGetNamedBufferSubData(iota, 0, sizeof(uint32_t) * result.size(), result.data());
-    for (int i = 0; i<1024*1024; i+=64*1024)
-        printf("%u\n", result[i]);
+    TimeStamp start;
+    while (loop()) {
+        TimeStamp now;
+        float t = .001f*(float)cpuTime(start, now);
+
+        auto vert = [&] {
+            out vec3 col;
+            uniform float bind(t);
+            in vec3 bind_attribute(position, color);
+            void glsl_main() {
+                col = color;
+                float c = cos(t), s = sin(t);
+                gl_Position = vec4(mat2(c,s,-s,c)*position.xy*vec2(9.f/16.f,1.f), position.z, 1.f);
+            }
+        };
+        auto frag = [] {
+            in vec3 col;
+            out vec3 screen;
+            void glsl_main() {
+                screen = col;
+            }
+        };
+
+        useShader(vert(), frag());
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        swapBuffers();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+    return 0;
 }
