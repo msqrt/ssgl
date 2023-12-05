9b037a9bdb0fcd0d24c2fc659d9de942a2e8d087
msqrt <pauli.kemppinen@aalto.fi>
2023-12-05 23:17:09 +0200

    going fast

--- main.cpp
+++ main.cpp
@@ -1,61 +1,105 @@
 
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
 #include "ssgl.h"
 #include "gl_timing.h"
 
 int main() {
-    // init OpenGL, create window
-    OpenGL context(1280, 720, "Triangle");
+    
+    LARGE_INTEGER cpu_start, cpu_end, freq;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&cpu_start);
 
-    // define some vertices as a CPU array
-    struct Vertex { vec2 position; vec3 color; };
-    Vertex verts[] = {
-        {vec2(.5f, .0f), vec3(1.f, .0f, .0f)},
-        {vec2(-.4f, .5f), vec3(.0f, 1.f, .0f)},
-        {vec2(-.4f,-.5f), vec3(.0f, .0f, 1.f)}
+    std::ifstream file("input.txt");
+    std::vector<std::string> lines;
+    for (std::string line; std::getline(file, line); lines.push_back(line));
+    
+    auto extract = [](std::string& s) {
+        auto len = s.find(' ');
+        if(len == std::string::npos) len = s.length();
+        auto result = s.substr(0, len);
+        s = s.substr(std::min(s.length(), len+1));
+        return std::stoll(result);
     };
 
-    // send the vertices to the GPU
-    Buffer b; glNamedBufferData(b, sizeof(Vertex) * 3, verts, GL_STATIC_DRAW);
-    // set up attributes corresponding to the values
-    Attribute<vec2> position(b, sizeof(Vertex), 0);
-    Attribute<vec3> color(b, sizeof(Vertex), sizeof(vec2));
+    std::vector<i64vec2> seeds;
+    lines[0] = lines[0].substr(7);
+    while (lines[0].length())
+        seeds.push_back({ extract(lines[0]), extract(lines[0]) });
+    
+    std::vector<i64vec4> ranges;
+    for (int i = 1; i < lines.size(); ++i)
+        if (lines[i].length() == 0)
+            for(auto& r : ranges) { if(r.w == -1) r.w = ranges.size(); }
+        else if (!std::isalpha(lines[i][0]))
+            ranges.push_back({ extract(lines[i]), extract(lines[i]), extract(lines[i]), -1 });
 
-    // start timing
+    std::vector<int64_t> result = {std::numeric_limits<int64_t>::max()};
+
+    OpenGL context(1280, 720, "AoC 2023 Day 5 part 2", false, false);
+    
+    Buffer seed_buffer(seeds), range_buffer(ranges), result_buffer(result);
+
+    auto run = [&] {
+        glsl_extension(GL_NV_shader_atomic_int64, require);
+        layout (local_size_x = 1024) in;
+        buffer bind_block(seed_buffer) { dynamic_array(i64vec2, seeds); };
+        buffer bind_block(range_buffer) { dynamic_array(i64vec4, ranges); };
+        buffer bind_block(result_buffer) { int64_t result; };
+
+        shared i64vec2 sseeds[10];
+        shared i64vec4 sranges[233];
+
+        void glsl_main() {
+            uint lid = gl_LocalInvocationIndex;
+            if (lid < 10) sseeds[lid] = seeds[lid];
+            if (lid < 233) sranges[lid] = ranges[lid];
+            barrier();
+
+            int64_t smallest = result, threads = int64_t(gl_NumWorkGroups.x * gl_WorkGroupSize.x);
+            for (int64_t id = int64_t(gl_GlobalInvocationID.x); ; id += threads) {
+                int64_t seed = id;
+                for (int k = 0; k < seeds.length(); ++k) {
+                    if (seed < sseeds[k].y) {
+                        seed += sseeds[k].x;
+                        break;
+                    }
+                    else seed -= sseeds[k].y;
+                    if(k+1 == seeds.length())
+                        seed = -1;
+                }
+                if(seed < 0) break;
+
+                for (int k = 0; k < ranges.length() && k>=0;) {
+                    i64vec4 r = sranges[k];
+                    if (seed >= r.y && seed < r.y + r.z) {
+                        seed = r.x + seed - r.y;
+                        k = int(r.w);
+                    }
+                    else ++k;
+                }
+                smallest = min(smallest, seed);
+            }
+
+            atomicMin(result, smallest);
+        }
+    };
     TimeStamp start;
 
-    // while window open and ESC not pressed
-    while (loop()) {
-        // get time elapsed
-        TimeStamp now;
-        float t = .001f*(float)cpuTime(start, now);
+    useShader(run());
+    glDispatchCompute(200, 1, 1);
 
-        // define the shaders
-        auto vertex = [&] {
-            uniform float bind(t);
-            in vec2 bind_attribute(position);
-            in vec3 bind_attribute(color);
-            out vec3 col;
-            void glsl_main() {
-                col = color;
-                float c = cos(t), s = sin(t);
-                gl_Position = vec4(mat2(c, s, -s, c) *position.xy*vec2(9.f/16.f,1.f), .0f, 1.f);
-            }
-        };
-        auto fragment = [&] {
-            in vec3 col;
-            out vec3 bind_target(screen); // screen is the special name
-            void glsl_main() {
-                screen = col;
-            }
-        };
+    TimeStamp end;
 
-        // set up the shader and draw
-        useShader(vertex(), fragment());
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+    result = result_buffer;
 
-        // show the frame and clear the screen
-        swapBuffers();
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    }
+    std::cout << result[0] << "\n";
+    QueryPerformanceCounter(&cpu_end);
+
+    std::cout << "compute in " << gpuTime(start, end) << "ms\n";
+    std::cout << "whole program in " << (double(cpu_end.QuadPart - cpu_start.QuadPart)/double(freq.QuadPart)*1000.) << "ms\n";
+
     return 0;
 }
